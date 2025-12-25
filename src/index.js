9eac59838efe64b72f5dfa984c21c1af3fe0b535
Rayan37307 <r11137307@gmail.com>
2025-12-25 20:56:14 +0600

    util code and custom api error and response

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
 dotenv.config()
-
+import e from 'express'
+const app = e()
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8080,() => {
+        console.log(`Server is running on http:/localhost:${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("mongodb connection failed", err);
+    
+})
